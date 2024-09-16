335e4e5c5eac469680e5b0b9f44b6e1c3ded74dc
forswear <adml90@mail.ru>
2024-09-16 16:37:38 +0700

    update_task5

--- src/even.js
+++ src/even.js
@@ -1,44 +1,46 @@
 import readlineSync from 'readline-sync';
- 
+
 // Определение функции запроса имени
 
 console.log("Welcome to the Brain Games!");
 
 const getName = () => readlineSync.question('May I have your name? ');
- 
+
 const userName = getName();
- 
+
 // Определение функции приветствия
 export const greeting = () => {
   console.log(`Hello, ${userName}!`);
 };
- 
+
 // Определение функции, дающее рандомное целое число
 const getRandomInt = (max) => {
   return Math.floor(Math.random() * max);
 };
- 
+
 // Определение функции игры на четность
 export const evenGame = () => {
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
- 
+
   for (let i = 0; i < 3; i += 1) {
     const number = getRandomInt(100);
     console.log('Question: ', number);
     const answerUser = readlineSync.question('Your answer: ');
- 
+
     const checking1 = (number % 2 === 0) && (answerUser === 'yes');
     const checking2 = (number % 2 !== 0) && (answerUser === 'no');
- 
+
     if (checking1 === true || checking2 === true) {
       console.log('Correct!');
     } else {
       const opposite = (answerUser === 'yes' ? 'no' : 'yes');
-      const errorMessage = `"${answerUser}" is wrong answer ;(. Correct answer was '${opposite}'. /n Let's try again, ${userName}!`;
-      return errorMessage;
+      const errorMessage = `"${answerUser}" is wrong answer ;(. Correct answer was '${opposite}'.\nLet's try again, ${userName}!`;
+      console.error(errorMessage);
+      break;
+      
     }
+console.log(`Congratulations, ${userName}!`);
   }
- 
-  console.log(`Congratulations, ${userName}!`);
+
+  
 };
- 
